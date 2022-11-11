32735d5d24ff76368076fed9d61770f3e69028d8
Zeerex23 <tobilobadiekola@gmail.com>
2022-11-11 22:36:41 +0100

    feat: added 'Header', 'Home' & 'Footer' components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useLocation } from 'react-router-dom';
 
 import Routes from './Routes';
+import { Header, Footer } from './components';
 import './assets/styles.css';
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
+      <Header />
+
       <Routes location={location} />
+
+      <Footer />
     </div>
   );
 }
