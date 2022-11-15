727dff64575c506edc0b6d6c9ede0864171187ed
Zeerex23 <tobilobadiekola@gmail.com>
2022-11-15 01:57:16 +0100

    feat: added Navigation Bar to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,79 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+import useStyles from './styles'
 
 const Header = () => {
+  const classes = useStyles();
+  
   return (
-    <>
-      
-    </>
+    <div className={classes.navBar}>
+      <div className={classes.brandLogo}>
+        
+      </div>
+
+      <div className={classes.navLinks}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => 
+          [
+            classes.navLinkItem,
+            isActive ? classes.activeNavLink : undefined,
+          ]
+          .filter(Boolean)
+          .join(" ")
+        }
+          end
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/#about"
+          className={({ isActive }) => 
+          [
+            classes.navLinkItem,
+            isActive ? classes.activeNavLink : undefined,
+          ]
+          .filter(Boolean)
+          .join(" ")
+        }
+          end
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          to="/#skills"
+          className={({ isActive }) => 
+          [
+            classes.navLinkItem,
+            isActive ? classes.activeNavLink : undefined,
+          ]
+          .filter(Boolean)
+          .join(" ")
+        }
+          end
+        >
+          Skills
+        </NavLink>
+
+        <NavLink
+          to="/#contact"
+          className={({ isActive }) => 
+          [
+            classes.navLinkItem,
+            isActive ? classes.activeNavLink : undefined,
+          ]
+          .filter(Boolean)
+          .join(" ")
+        }
+          end
+        >
+          Contact
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Header/styles.js
@@ -0,0 +1,57 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles( (theme) => ({
+  navBar: {
+    width: "100%",
+    height: "7.8%",
+    backgroundColor: '#211E1E',
+    padding: '2.65% 0',
+    display: "flex",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  brandLogo: {
+    height: "75px",
+    width: "75px",
+    backgroundColor: "white"
+  },
+
+  navLinks: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "20%",
+    height: "45%",
+    // marginRight: "10%"
+  },
+
+  navLinkItem: {
+    textDecoration: 'none',
+    color: '#ffffff',
+    fontFamily: 'Montserrat',
+    fontSize: '14px',
+    fontWeight: '350',
+    height: '100%',
+    overflow: 'hidden',
+    position: 'relative',
+    transition: 'all 0.5s ease-in-out',
+
+    '&:after': {
+      content: '""',
+      position: 'absolute',
+      height: '2px',
+      width: '100%',
+      bottom: '0',
+      left: '0',
+      borderRadius: '30px',
+      backgroundColor: 'white',
+      transform: 'translateX(-100%)',
+      transition: 'all 0.5s ease-in-out',
+    },
+
+    '&:hover:after, &:focus:after': {
+      transform: 'translateX(0%)'
+    },
+  },
+}))
