d359d094b06d4abd23e53e6bbb242c650b531110
Zeerex23 <tobilobadiekola@gmail.com>
2022-11-16 04:49:46 +0100

    refactor: added index.js to export assets from the assets directory

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import Logo1 from './images/web_icon-01.png';
+import Logo2 from './images/web_icon-02.png';
+import Logo3 from './images/web_icon-03.png';
+import Logo4 from './images/web_icon-04.png';
+
+export { Logo1, Logo2, Logo3, Logo4 };
