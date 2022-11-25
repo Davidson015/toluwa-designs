c936cf8a4211b766b9523051f0635364aac89d5a
Zeerex23 <tobilobadiekola@gmail.com>
2022-11-25 18:10:14 +0100

    feat: added mock project images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,13 @@ import Logo2 from './images/web_icon-02.png';
 import Logo3 from './images/web_icon-03.png';
 import Logo4 from './images/web_icon-04.png';
 
-export { Logo1, Logo2, Logo3, Logo4 };
+import p1 from './images/projectImages/makari.jpg';
+import p2 from './images/projectImages/amnHealth.jpg';
+import p3 from './images/projectImages/sixMonthSmiles.jpg';
+import p4 from './images/projectImages/bSpoke.jpg';
+import p5 from './images/projectImages/paychex.jpg';
+import p6 from './images/projectImages/logos.jpg';
+
+const projects = [p1, p2, p3, p4, p5, p6];
+
+export { Logo1, Logo2, Logo3, Logo4, projects };
